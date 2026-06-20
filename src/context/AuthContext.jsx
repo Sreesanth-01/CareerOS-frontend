23ff7119cb36882e21f4a597722e1bcc94ec41ee
Sreesanth-01 <iamshanth7@gmail.com>
2026-06-20 14:04:16 +0530

    feat: AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from 'react'
+
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+
+    const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem("token"));
+    const [userEmail, setUserEmail] = useState("");
+
+    const login = (token,email) =>{
+        localStorage.setItem("token",token);
+        localStorage.setItem("email",email);
+        setIsAuthenticated(true);
+        setUserEmail(email);
+    }
+
+    const logout = () =>{
+        localStorage.removeItem("token");
+        localStorage.removeItem("email");
+        setIsAuthenticated(false);
+        setUserEmail("");
+    }
+
+  return (
+    <AuthContext.Provider value={{isAuthenticated,login,logout,userEmail}}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext
