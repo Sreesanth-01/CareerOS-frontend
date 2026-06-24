5836f8aa272f088b5d4cb2e8c2aa13f25606834c
Sreesanth-01 <iamshanth7@gmail.com>
2026-06-24 14:30:41 +0530

    integration: paths and working

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,9 +1,9 @@
 import api from "./axios";
 
 export const registerUser = (data) =>{
-    return api.post('/path',data);
+    return api.post('/auth/signUp',data);
 }
 
 export const login = (data) =>{
-    return api.post("/path",data);
+    return api.post("/auth/login",data);
 }
