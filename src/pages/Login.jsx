cad0c3c4d3ed460eb1e8520bca9afbb11d96a636
Sreesanth-01 <iamshanth7@gmail.com>
2026-08-31 17:24:49 +0530

    style: Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -37,12 +37,17 @@ const Login = () => {
 
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <Input name="email" value={form.email} label="Email:" onChange={handleChange} placeholder="Enter email" type='email'></Input>
-        <Input name="password" value={form.password} label="Password:" onChange={handleChange} placeholder="******" type='password'></Input>
-        <button type='submit'>Login</button>
-      </form>
+    <div className='min-h-screen items-center justify-center px-6 flex flex-col'>
+      <div>
+        <h1 className='text-4xl font-semibold text-center mb-6 mt-6'>Login</h1>
+      </div>
+      <div className='w-full max-w-md  rounded-md shadow-sm'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          <Input name="email" value={form.email} label="Email:" onChange={handleChange} placeholder="Enter email" type='email'></Input>
+          <Input name="password" value={form.password} label="Password:" onChange={handleChange} placeholder="******" type='password'></Input>
+          <button type='submit' className='w-full bg-black text-white py-2 px-4 rounded-md font-medium hover:font-bold transition duration-500 cursor-pointer'>Login</button>
+        </form>
+      </div>
     </div>
   )
 }
