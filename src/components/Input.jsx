22c8f1e16fc110f3306ba799152a4842910b4a2e
Sreesanth-01 <iamshanth7@gmail.com>
2026-08-31 11:45:57 +0530

    style: Register page

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Input = ({label, name, type="text", value, onChange, placeholder}) => {
   return (
-    <div>
-      <label>{label}</label>
-      <input name={name} type={type} value={value} onChange={onChange} placeholder={placeholder}></input>
+    <div className='flex flex-col gap-1'>
+      <label className='text-md font-medium'>{label}</label>
+      <input className='w-full border border-gray-300 rounded-md px-3 py-2 outline-none focus:ring-1 focus:ring-black-500 focus-border-black-500' name={name} type={type} value={value} onChange={onChange} placeholder={placeholder}></input>
     </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -29,17 +29,17 @@ const Register = () => {
   }
 
   return (
-    <div>
+    <div className='min-h-screen items-center justify-center px-6 flex flex-col'>
       <div>
-        <h1 className='text-3xl font-bold text-center mb-6'>Register</h1>
+        <h1 className='text-4xl font-semibold text-center mb-6 mt-6'>Register</h1>
       </div>
-      <div className='flex fex-col'>
-        <form onSubmit={handleSubmit}>
+      <div className='w-full max-w-md  rounded-md shadow-sm'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
             <Input label="UserName" name="userName" value={form.userName} type="text" onChange={handleChange} placeholder="Enter Username"></Input>
             <Input label="Email" name="email" value={form.email} type="email" onChange={handleChange} placeholder="you@example.com"></Input>
             <Input label="Mobile" name="mobile" value={form.mobile} type="text" onChange={handleChange} placeholder="Your mobile no."></Input>
             <Input label="Password" name="password" value={form.password} type="password" onChange={handleChange} placeholder="Enter Password"></Input>
-            <button type='submit'>Register</button>
+            <button type='submit' className='w-full bg-black text-white py-2 px-4 rounded-md font-medium hover:font-bold transition duration-500 cursor-pointer'>Register</button>
         </form>
       </div>
     </div>
