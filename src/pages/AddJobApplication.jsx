8d8f4517095f8908f8e0ccb7705bf496217b58db
Sreesanth-01 <iamshanth7@gmail.com>
2026-09-01 12:11:34 +0530

    style: AddJobs page

--- src/pages/AddJobApplication.jsx
+++ src/pages/AddJobApplication.jsx
@@ -33,26 +33,34 @@ const AddJobApplication = () => {
     }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <Input name="companyName" value={formData.companyName} label="Company" placeholder="Company Name" onChange={handleChange}></Input>
-        <Input name="jobRole" value={formData.jobRole} label="Role" placeholder="Job Role" onChange={handleChange}></Input>
-        <Input name="salary" value={formData.salary} label="Salary" placeholder="Salary" type='number' onChange={handleChange}></Input>
-        <select name="status" value={formData.status} onChange={handleChange}>
-            <option value="SAVED">Saved</option>
-            <option value="APPLIED">Applied</option>
-            <option value="ASSESSMENT">Assessment</option>
-            <option value="INTERVIEW">Interview</option>
-            <option value="OFFER_RECIEVED">Offer Recieved</option>
-            <option value="ACCEPTED">Accepted</option>
-            <option value="REJECTED">Rejected</option>
-            <option value="WITHDRAWN">Withdrawn</option>
-            <option value="ON_HOLD">On Hold</option>
-        </select>
+    <div className='min-h-screen flex flex-col'>
+      <div>
+        <h1 className='text-center text-2xl font-semibold m-6'>Add Job Application</h1>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          <Input name="companyName" value={formData.companyName} label="Company" placeholder="Company Name" onChange={handleChange}></Input>
+          <Input name="jobRole" value={formData.jobRole} label="Role" placeholder="Job Role" onChange={handleChange}></Input>
+          <Input name="salary" value={formData.salary} label="Salary" placeholder="Salary" type='number' onChange={handleChange}></Input>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='status' className='font-semibold'>Status</label>
+            <select name="status" value={formData.status} onChange={handleChange} className='w-full border border-gray-300 rounded-md px-3 py-2 outline-none focus:ring-1 focus:ring-black-500 focus-border-black-500'>
+              <option value="SAVED">Saved</option>
+              <option value="APPLIED">Applied</option>
+              <option value="ASSESSMENT">Assessment</option>
+              <option value="INTERVIEW">Interview</option>
+              <option value="OFFER_RECIEVED">Offer Recieved</option>
+              <option value="ACCEPTED">Accepted</option>
+              <option value="REJECTED">Rejected</option>
+              <option value="WITHDRAWN">Withdrawn</option>
+              <option value="ON_HOLD">On Hold</option>
+          </select>
+        </div>
         <Input name="appliedDate" value={formData.appliedDate} label="Applied Date" type='date' placeholder="Applied Date" onChange={handleChange}></Input>
         <Input name="notes" value={formData.notes} label="Notes" placeholder="Add notes" onChange={handleChange}></Input>
-        <button type='submit'>Add Application</button>
+        <button type='submit' className='w-full bg-black text-white py-2 px-4 rounded-md font-medium hover:font-bold transition duration-500 cursor-pointer'>Add Application</button>
       </form>
+    </div>
       {/* <button onClick={()=>navigate("/viewJobApplications")}>view jobs</button> */}
     </div>
   )
