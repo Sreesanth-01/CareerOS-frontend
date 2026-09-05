bb67c93c65f657f542f5ab2c5d184c0bad218c02
Sreesanth-01 <iamshanth7@gmail.com>
2026-09-05 12:40:48 +0530

    refactor: JobList Page-style

--- src/pages/ViewJobApplications.jsx
+++ src/pages/ViewJobApplications.jsx
@@ -38,12 +38,12 @@ const ViewJobApplications = () => {
     }
 
   return (
-    <div className='min-h-screen flex flex-col items-center justify-center'>
-        <div>
-            <h1 className='text-2xl font-semibold text-center'>Job List</h1>
+    <div className='min-h-screen bg-gray-50 py-10 px-4'>
+        <div className='max-w-3xl mx-auto'>
+            <h1 className='text-3xl font-bold mb-8 text-center'>Job List</h1>
         </div>
         <div className='w-full max-w-2-xl'>
-            <ul className='flex flex-col gap-4'>
+            <ul className='space-y-5'>
                 {jobData.map((job)=>(
                     <li key={job.id}>
                         <div className='bg-white border border-gray-200 rounded-2xl shadow-sm hover:shadow-lg transition duration-200 p-6'>
